69a5261cec72e674ea462c3acdc222a82cf10c57
Hegy <45619085+hegyok@users.noreply.github.com>
2026-01-20 18:18:49 +0100

    tests for API

---
+++ apps/api/tests/s3.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, it } from 'vitest';
+import S3Storage from '../src/util/s3';
+import fs from "fs"
+import path from "path";
+
+describe("S3Storage", () => {
+  const s3 = new S3Storage();
+  let uploadedUrl: string;
+  const testKey = `test-upload-${Date.now()}.jpeg`;
+
+  it("should upload an image to S3", async () => {
+    // Load the test image
+    const imagePath = path.resolve(__dirname, "../../../static/cat.jpeg");
+    const imageBuffer = fs.readFileSync(imagePath);
+
+    // Upload the test image to s3
+    const response = await s3.upload(imageBuffer, testKey, "image/jpeg");
+    
+    // Get the image and set it globally
+    uploadedUrl = response.url;
+
+    expect(uploadedUrl).not.toBeNull();
+    expect(uploadedUrl).toContain(testKey);
+    expect(response.response.$metadata.httpStatusCode).toBe(200);
+  })
+
+  it("should delete an image from S3", async () => {
+      // Delete the image we just uploaded
+      const response = await s3.delete(testKey);
+      
+      expect(response.$metadata.httpStatusCode).toBe(204);
+  })
+})
---
+++ apps/api/vitest.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vitest/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { loadEnvFile } from 'process';
+import fs from 'fs';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Load the env for tests if it exists
+const envPath = path.resolve(__dirname, '../../.env.test');
+if (fs.existsSync(envPath)) {
+  loadEnvFile(envPath);
+}
+
+export default defineConfig({
+  test: {
+    name: "api",
+    env: {
+      // set the env
+      ...process.env
+    },
+    testTimeout: 60000 // Increase timeout for model loading
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  },
+});
